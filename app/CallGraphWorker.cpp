6b2d72dccce3c9be5aa5897cc1f68332ff7a8943
omvs077 <omvs077@gmail.com>
2026-07-10 18:08:50 +0530

    Add CallGraphWorker (Qt threading layer for CallGraphAnalyzer)

---
+++ app/CallGraphWorker.cpp
@@ -0,0 +1,17 @@
+﻿#include "CallGraphWorker.h"
+
+CallGraphWorker::CallGraphWorker(std::filesystem::path root,
+                                  std::vector<std::filesystem::path> sourceFiles,
+                                  CodeGraph baseGraph,
+                                  QObject* parent)
+    : QObject(parent)
+    , m_root(std::move(root))
+    , m_sourceFiles(std::move(sourceFiles))
+    , m_baseGraph(std::move(baseGraph)) {
+    qRegisterMetaType<CallGraphResult>("CallGraphResult");
+}
+
+void CallGraphWorker::run() {
+    CallGraphResult result = CallGraphAnalyzer::analyze(m_root, m_sourceFiles, m_baseGraph);
+    emit finished(result);
+}
---
+++ app/include/CallGraphWorker.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <QObject>
+#include <QMetaType>
+#include <filesystem>
+#include <vector>
+#include "CallGraphAnalyzer.h"
+
+Q_DECLARE_METATYPE(CallGraphResult)
+
+class CallGraphWorker : public QObject {
+    Q_OBJECT
+public:
+    CallGraphWorker(std::filesystem::path root,
+                     std::vector<std::filesystem::path> sourceFiles,
+                     CodeGraph baseGraph,
+                     QObject* parent = nullptr);
+
+public slots:
+    void run();
+
+signals:
+    void finished(CallGraphResult result);
+
+private:
+    std::filesystem::path m_root;
+    std::vector<std::filesystem::path> m_sourceFiles;
+    CodeGraph m_baseGraph;
+};
